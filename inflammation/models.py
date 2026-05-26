c5bbb83c4de38c79fed52c58bf73d1a860bd4014
Habtemariam <h.a.kefale@tudelft.nl>
2026-05-26 14:27:44 +0200

    added type hints

--- inflammation/models.py
+++ inflammation/models.py
@@ -10,7 +10,7 @@ and each column represents a single day across all patients.
 import numpy as np
 
 
-def load_csv(filename):  
+def load_csv(filename: str) -> np.ndarray:
     """Load a Numpy array from a CSV
 
     :param filename: Filename of CSV to load
@@ -18,17 +18,17 @@ def load_csv(filename):
     return np.loadtxt(fname=filename, delimiter=',')
 
 
-def daily_mean(data):
+def daily_mean(data: np.ndarray) -> np.ndarray:
     """Calculate the daily mean of a 2d inflammation data array."""
     return np.mean(data, axis=0)
 
 
-def daily_max(data):
+def daily_max(data: np.ndarray) -> np.ndarray:
     """Calculate the daily max of a 2d inflammation data array."""
     return np.max(data, axis=0)
 
 
-def daily_min(data):
+def daily_min(data: np.ndarray) -> np.ndarray:
     """Calculate the daily min of a 2d inflammation data array."""
     return np.min(data, axis=0)
 
