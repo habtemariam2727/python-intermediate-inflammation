b7ed5deba72590d2a0e7c066c51490a277c4ce50
Habtemariam <h.a.kefale@tudelft.nl>
2026-05-26 13:21:22 +0200

    check daily mean

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,11 +2,12 @@
 
 import numpy as np
 import numpy.testing as npt
+import pytest
 
 from inflammation.models import daily_mean
 from inflammation.models import daily_max
 from inflammation.models import daily_min   
-
+from inflammation.models import daily_mean
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
     
@@ -42,6 +43,19 @@ def test_daily_max_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_max(test_input), test_result)
 
+
+# daily-mean test
+def test_daily_mean():
+    """Test that mean function works for an array of positive integers."""
+
+    test_input = np.array([[1, 2],
+                           [3, 4],
+                           [5, 6]])
+    test_result = np.array([3, 4])
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_mean(test_input), test_result)
+
 def test_daily_min_integers():
     """Test that min function works for an array of positive integers."""
 
@@ -52,3 +66,12 @@ def test_daily_min_integers():
 
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_min(test_input), test_result)
+
+
+
+def test_daily_max_string():
+    ''' Test the max function with an array of strings.'''
+    with pytest.raises(TypeError):
+        error_expected = daily_max(['hello', 'there', 'world'])
+
+
