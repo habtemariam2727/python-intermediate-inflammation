70be1f0b203b7a9294370552109f2fe5926f5684
Habtemariam <h.a.kefale@tudelft.nl>
2026-05-27 09:33:30 +0200

    replace visualize to print

--- inflammation/analysis.py
+++ inflammation/analysis.py
@@ -18,18 +18,30 @@ def analyse_data(data_dir):
     data_file_paths = glob.glob(os.path.join(data_dir, 'inflammation*.csv'))
     if len(data_file_paths) == 0:
         raise ValueError(f"No inflammation data CSV files found in path {data_dir}")
+    
+    '''
+    map is a built-in function that applies a given function to each item of an iterable 
+    (like a list) and returns an iterator.
+    '''
     data = map(models.load_csv, data_file_paths)
 
 
     means_by_day = map(models.daily_mean, data)
     means_by_day_matrix = np.stack(list(means_by_day))
 
-    daily_standard_deviation = np.std(means_by_day_matrix, axis=0)
+    daily_standard_deviation = np.std(means_by_day_matrix, axis=0)  
 
-    graph_data = {
-        'standard deviation by day': daily_standard_deviation,
-    }
-    views.visualize(graph_data)
+
+    print("Standard deviation by day:", daily_standard_deviation)
+
+    return daily_standard_deviation
+
+
+
+    # graph_data = {
+    #     'standard deviation by day': daily_standard_deviation,
+    # }
+    # views.visualize(graph_data)
 
 if __name__ == '__main__':
     # Initialize the parser
